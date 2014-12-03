4ec2b94551858e404f0de6d8ad3827d9c6138491
David Cuthbert <dacut@kanga.org>
2014-12-03 15:57:13 -0800

    Fix syntax errors. (preventing install)

--- slurmec2utils/sysinit.py
+++ slurmec2utils/sysinit.py
@@ -5,4 +5,6 @@ from boto.s3.key import Key
 from .clusterconfig import ClusterConfiguration
 from .instanceinfo import get_instance_id
 
-def check_munge_
+def get_munge_key(cluster_configuration=None):
+    if cluster_configuration is None:
+        cluster_configuration = ClusterConfiguration()
